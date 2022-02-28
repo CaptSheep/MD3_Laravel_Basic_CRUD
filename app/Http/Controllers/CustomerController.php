a0f6ba7f3889ab1178f3fd7983d0c11afb1c4347
trieu.long <long.trieu.hr@gmail.com>
2022-02-28 09:56:17 +0700

    basic CRUD

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        $customers = DB::table('customers')->get();
+        return view('modules.customer.index', compact('customers'));
+    }
+
+    public function create()
+    {
+        return view('modules.customer.create');
+    }
+
+
+    public function store(Request $request)
+    {
+        $data = $request->only('name', 'phone', 'email');
+        DB::table('customers')->insert($data);
+        return redirect(route('index'));
+    }
+
+
+    public function show($id)
+    {
+        $customers = DB::table('customers')->where('id', $id)->get();
+//        dd($customers);
+        return view('modules.customer.show', compact('customers'));
+    }
+
+
+    public function edit($id)
+    {
+        $customers = DB::table('customers')->where('id', $id)->first();
+        return view('modules.customer.edit', compact('id', 'customers'));
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->only('name', 'phone', 'email');
+        DB::table('customers')->where('id', $id)->update($data);
+        return redirect()->route('index');
+
+    }
+
+
+    public function destroy($id)
+    {
+        DB::table('customers')->where('id', $id)->delete();
+        return redirect()->route('index');
+    }
+}
---
+++ resources/views/modules/customer/index.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<table border="1" style="text-align: center">
+    <a href="{{route('createForm')}}">Create Customers</a>
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Customer Name</th>
+        <th colspan="4">Action</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($customers as $customer)
+        <tr>
+            <td>{{$customer->id}}</td>
+            <td>{{$customer->name}}</td>
+            <td><a href="{{route('show',$customer->id)}}">Customer Detail</a></td>
+            <td><a href="{{route('edit',$customer->id)}}">Edit Customers</a></td>
+{{--            <td><a href="{{route('update',$customer->id)}}">upd Customers</a></td>--}}
+            <td><a onclick="return confirm('Are you sure?')" href="{{route('destroy',$customer->id)}}">Delete</a></td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
+</body>
+</html>
